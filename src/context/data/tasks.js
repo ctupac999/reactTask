cf0b569c421ca188a7566055f7f27c298422ef2d
ctupac999 <critiands9999@gmail.com>
2024-11-15 17:27:05 +0100

    Ok

---
+++ src/context/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title: "tarea uno",
+        description: "descripcion de la tarea uno"
+    },
+    {
+        id:1,
+        title: "tarea dos",
+        description: "descripcion de la tarea dos"
+    },
+    {
+        id:2,
+        title: "tarea tres",
+        description: "descripcion de la tarea tres"
+    }
+]
